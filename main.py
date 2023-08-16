0a0c1e2eeb7ec39c0e20c295d05ce061194e0d9f
John Tran <johntr2002@gmail.com>
2023-08-15 22:14:53 -0400

    created a new frame and changed background colors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from tkinter import *
 
 root = Tk()
+root.configure(bg="black")
 root.geometry('1440x720')
 root.title("Minesweeper")
 root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='red',  # change to black later
+    width=1440,
+    height=180
+)
+top_frame.place(x=0, y=0)
+
 root.mainloop()
