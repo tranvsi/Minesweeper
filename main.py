8f67794e99298664c6833ed419f7904d0465f564
John Tran <johntr2002@gmail.com>
2023-08-16 11:54:37 -0400

    created a static method that randomizes mines

--- main.py
+++ main.py
@@ -39,12 +39,12 @@ center_frame.place(
 
 for x in range(settings.GRID_SIZE):
     for y in range(settings.GRID_SIZE):
-        c = Cell()
+        c = Cell(x, y)
         c.create_btn_object(center_frame)
         c.cell_btn_object.grid(
             column=x, row=y
         )
 
-
+Cell.randomize_mines()
 
 root.mainloop()
