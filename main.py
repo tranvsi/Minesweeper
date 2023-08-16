0971fc457a661c45d6723aa4723c91bd1ea5718f
John Tran <johntr2002@gmail.com>
2023-08-15 22:24:31 -0400

    created utils.py for calculating height and width

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import settings
+import utils
 
 root = Tk()
 root.configure(bg="black")
@@ -10,16 +11,16 @@ root.resizable(False, False)
 top_frame = Frame(
     root,
     bg='red',  # change to black later
-    width=1440,
-    height=180
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
 )
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
     bg='blue',
-    width=360,
-    height=540
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
 )
 left_frame.place(x=0, y=180)
 
