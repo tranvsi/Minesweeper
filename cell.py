724bbe489fcfab87a3dac84bc81b8c4083d91fb6
John Tran <johntr2002@gmail.com>
2023-08-22 13:39:59 -0400

    created algorithms to display mines around button clicked

--- cell.py
+++ cell.py
@@ -1,9 +1,12 @@
 from tkinter import Button
 import random
 
+import settings
+
 
 class Cell:
     all = []
+
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
@@ -18,15 +21,48 @@ class Cell:
             location,
             width=12,
             height=4,
-            text=f"{self.x}, {self.y}"
+
         )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-2>', self.right_click_actions)
         self.cell_btn_object = btn
 
     def left_click_actions(self, event):
-        print(event)
-        print("I am left clicked!")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+
+    def get_cell_by_axis(self, x, y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1),
+        ]
+
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+
+    def show_cell(self):
+        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+
+    def show_mine(self):
+        self.cell_btn_object.configure(bg='red')
 
     def right_click_actions(self, event):
         print(event)
@@ -35,7 +71,7 @@ class Cell:
     @staticmethod
     def randomize_mines():
         picked_cells = random.sample(
-            Cell.all, 9
+            Cell.all, settings.MINES_COUNT
 
         )
         for picked_cells in picked_cells:
