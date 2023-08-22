cda7584cb274ddea646f14a45312cf9359bd5f25
John Tran <johntr2002@gmail.com>
2023-08-22 14:16:15 -0400

    opens buttons surrounding if there are no mines around it

--- cell.py
+++ cell.py
@@ -1,13 +1,16 @@
-from tkinter import Button
+from tkinter import Button, Label
 import random
 import settings
 
 
 class Cell:
     all = []
+    cell_count = settings.CELL_COUNT
+    cell_count_label_object = None
 
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
+        self.is_opened = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
@@ -26,10 +29,24 @@ class Cell:
         btn.bind('<Button-2>', self.right_click_actions)
         self.cell_btn_object = btn
 
+    @staticmethod
+    def create_cell_count_label(location):
+        lbl = Label(
+            location,
+            bg='black',
+            fg='white',
+            text=f"Cells Left:{Cell.cell_count}",
+            font=("", 30)
+        )
+        Cell.cell_count_label_object = lbl
+
     def left_click_actions(self, event):
         if self.is_mine:
             self.show_mine()
         else:
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
             self.show_cell()
 
     def get_cell_by_axis(self, x, y):
@@ -63,7 +80,14 @@ class Cell:
         return counter
 
     def show_cell(self):
-        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+        if not self.is_opened:
+            Cell.cell_count -= 1
+            self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                    text=f"Cells Left:{Cell.cell_count}"
+                )
+        self.is_opened = True
 
     def show_mine(self):
         self.cell_btn_object.configure(bg='red')
